71e82611cda7897e67b86a8b9c8ba3accecc2ade
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2021-11-19 20:42:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-window.onload = function(){
-    var img = document.getElementById("arona_1");
-    var count = document.getElementById("score");
-    var score = 0;
-    var audio = new Audio("sonic-ring.mp3");
+var img = document.getElementById("arona_1");
+var count = document.getElementById("score");
+var score = 0;
+var audio = new Audio("sonic-ring.mp3");
 
-    img.addEventListener('mousedown', function (){
-        audio.play();
-        img.src = 'arona_2.jpeg';
-        increaseScore();
-    });
+img.addEventListener('mousedown', function (){
+    audio.play();
+    img.src = 'arona_2.jpeg';
+    increaseScore();
+});
 
-    img.addEventListener('mouseup', function (){
-        audio.play();
-        img.src = 'arona_1.jpeg';
-    });
+img.addEventListener('mouseup', function (){
+    audio.play();
+    img.src = 'arona_1.jpeg';
+});
 
-    function increaseScore(){
-        score++;
-        count.innerHTML = score;
-    }
+function increaseScore(){
+    score++;
+    count.innerHTML = score;
 }
