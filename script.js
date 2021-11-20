5ca2e54fcedf649f97b6d78cd65835cab3ac24db
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2021-11-19 22:32:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,29 +3,30 @@ var count = document.getElementById("score");
 var score = 0;
 var audio = new Audio("pop.mp3");
 
+function increaseScore(){
+    score++;
+    count.innerHTML = score;
+};
+
 img.addEventListener(('mousedown', 'touchstart'), function (){
     increaseScore();
     img.src = 'arona_2.png';
-    audio.play();
+    return audio.play();
 });
 
 img.addEventListener(('mouseup', 'touchend'), function (){
     img.src = 'arona_1.png';
-    audio.play();
+    return audio.play();
 });
 /*
 img.addEventListener('touchstart', function(){
     increaseScore();
     img.src = 'arona_2.png';
-    audio.play();
+    return audio.play();
 });
 
 img.addEventListener('touchend', function(){
     img.src = 'arona_1.png';
-    audio.play();
+    return audio.play();
 });
 */
-function increaseScore(){
-    score++;
-    count.innerHTML = score;
-};
