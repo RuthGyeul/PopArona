c3cfda126f16142560caf22ebdb5e3297f50fa7c
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2021-11-19 20:31:39 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+window.onload = function(){
+    var img = document.getElementById("arona_1");
+    var count = document.getElementById("score");
+    var score = 0;
+    var audio = new Audio("sonic-ring.mp3");
 
+    img.addEventListener('mousedown', function (){
+        increaseScore();
+        img.src = 'arona_2.jpeg';
+        audio.play();
+    });
+
+    img.addEventListener('mouseup', function (){
+        img.src = 'arona_1.jpeg';
+        audio.play();
+    });
+
+    function increaseScore(){
+        score++;
+        count.innerHTML = score;
+
+    }
+}
